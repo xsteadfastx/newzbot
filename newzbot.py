39c17ca35e8d3ad0aa3ee8dc1ac6feed4c1a4e79
Marvin Steadfast <xsteadfastx@gmail.com>
2013-02-14 15:00:02 +0100

    added two try exceptions

--- newzbot.py
+++ newzbot.py
@@ -13,21 +13,25 @@ class NewzBot(JabberBot):
     def idle_proc(self):
         self._idle_ping()
         global timecounter
-        if timecounter == 300:
-            timecounter = 1
-            for i in feedlist:
-                itemposition = feedlist.index(i)
+        try:
+            if timecounter == 300:
+                timecounter = 1
+                for i in feedlist:
+                    itemposition = feedlist.index(i)
 
-                new = feedparser.parse(i)
-                newlink = new['entries'][0]['link']
+                    new = feedparser.parse(i)
+                    newlink = new['entries'][0]['link']
 
-                if oldlink[itemposition] != newlink:
-                    newnews =  new['entries'][0]['title'] + ' ' + new['entries'][0]['link']
-                    for contact in self.roster.getRawRoster():
-                        self.send(contact, newnews)
-                    oldlink[itemposition] = newlink
-        else:
+                    if oldlink[itemposition] != newlink:
+                        newnews =  new['entries'][0]['title'] + ' ' + new['entries'][0]['link']
+                        for contact in self.roster.getRawRoster():
+                            self.send(contact, newnews)
+                        oldlink[itemposition] = newlink
+            else:
+                timecounter = timecounter + 1
+        except Exception:
             timecounter = timecounter + 1
+            pass
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -40,10 +44,13 @@ if __name__ == '__main__':
     feedlist = [line.strip() for line in open('./feeds.txt', 'r')] 
 
     oldlink = []
-    for i in feedlist:
-        oldfeed = feedparser.parse(i)
-        olditem = oldfeed['entries'][0]['link']
-        oldlink.append(olditem)
+    try:
+        for i in feedlist:
+            oldfeed = feedparser.parse(i)
+            olditem = oldfeed['entries'][0]['link']
+            oldlink.append(olditem)
+    except Exception:
+        pass
 
     username, password = sys.argv[1:]
     newz_bot = NewzBot(username, password, debug=True)
